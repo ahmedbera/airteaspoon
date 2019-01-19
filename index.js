f6250b58983696e3ce6321833d403a225e1086ea
Bera <ahmedbera@gmail.com>
2019-01-19 21:47:02 +0300

    update deadline messages

--- index.js
+++ index.js
@@ -37,28 +37,31 @@ client.on('message', message => {
             })
         } else if (params[1] == "deadline") {
             // @managersan deadline @user message {amount} {type}
-            let span = params.pop(); // span type ["days"]
-            if (span != "hour" || span != "day" || span != "week" || span != "month") {
-                message.channel.send("```Errör: Invalid parameter: 'type'. It must be one of [hour, day, week, month]```")
+            let span = params.pop(); // span type ["days"]            
+            if (span != "hour" && span != "day" && span != "week" && span != "minute") {
+                message.channel.send("```Errör: Invalid parameter: 'type'. It must be one of [minute, hour, day, week]```")
                 return;
             }
             let time = params.pop();
-            if (typeof (parseInt(time)) !== "number") {
-                message.channel.send("```Errör: Invalid parameter: 'amount'. It must be a number.```")
-            }
-            let message = params.slice(3, params.length - 2);
+            // TODO: make sure time is a Number
+            let msg = params.slice(3, params.length).join(" ");
             let user = params[2];
+            if(time * clock[span] >= 2147483647) {
+                message.channel.send("```Errör: Deadlines can't be longer than 24 days because of stupid js things```")
+                return;
+            }
             setTimeout(() => {
-                message.channel.send(user + " you have a reminder.\n```" + message + "```")
-            }, time * clock[span]);
+                // message.author.username
+                message.channel.send(user + " you have a reminder.\n```" + msg + " ```")
+            }, parseInt(time) * clock[span]);
         } else if (params[1] == "help") {
-            let helpMessage = ```md
-            ::Manager-san
-            + chapter   : 
-            example     : @Manager-san chapter hori 95
-            + deadline  : sets a deadline message for a user 
-            example     : @Manager-san deadline @user message 3 days
-            ```;
+            let helpMessage = "```"+
+            "\nManager-san" +
+            "\n  chapter   : Shows credits for specified chapter." +
+            "\n              @Manager-san chapter hori 95" +
+            "\n  deadline  : Pings user with message after specified time." +
+            "\n            : @Manager-san deadline @user message 3 days" +
+            "```"
             message.channel.send(helpMessage);
         }
     }
